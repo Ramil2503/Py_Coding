091dc4480257a351ca734ce6f5efd72af979bd9c
Ramil2503 <ramil.ax2503@gmail.com>
2023-06-12 00:59:52 +0400

    HW7, update

--- HW7/Task34.py
+++ HW7/Task34.py
@@ -1,3 +1,11 @@
+# Winnie the Pooh asked you to see if there is a rhythm in his poems. 
+# Since his chants are not as easy to understand as he makes them easy to understand, 
+# you should write a program. Winnie the Pooh believes that there is a rhythm 
+# if the number of syllables (i.e. the number of vowels) in each phrase of the poem is the same. 
+# A phrase can consist of one word, if there are several words in the phrase, then they are separated by hyphens. 
+# Phrases are separated from each other by spaces. Poem Winnie the Pooh drives into the program from the keyboard. 
+# In response, write “Param pam-pam” if the rhythm is okay and “Pam param” if the rhythm is not okay.
+
 def check_rythm(poem):
     arr = []
     words = poem.split(" ")
--- HW7/Task36.py
+++ HW7/Task36.py
@@ -1,3 +1,8 @@
+# Write a function print_operation_table(operation, num_rows=6, num_columns=6) 
+# that takes as an argument a function that calculates an element by row and column number. 
+# The num_rows and num_columns arguments specify the number of table rows and columns to be printed. 
+# The numbering of rows and columns starts from one (think why not from zero).
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for row in range(1, num_rows + 1):
         for column in range(1, num_columns + 1):
